ae8ec52c2448d61be1e0b5390acd04414a903094
John Watson <john.watson1@defra.gov.uk>
2025-04-02 15:03:47 +0100

    Add mTLS to client

--- client/index.js
+++ client/index.js
@@ -1,5 +1,18 @@
+import https from 'https'
+import fs from 'fs'
 import Wreck from '@hapi/wreck'
 
-const { payload } = await Wreck.get('http://localhost:3000', { json: true })
+const clientCert = fs.readFileSync('../certificates/client.crt')
+const clientKey = fs.readFileSync('../certificates/client-private-key.pem')
+const rootCA = fs.readFileSync('../certificates/rootCA.crt')
+
+const agent = new https.Agent({
+  cert: clientCert,
+  key: clientKey,
+  ca: rootCA,
+  rejectUnauthorized: true
+})
+
+const { payload } = await Wreck.get('http://localhost:3000', { agents: { https: agent }, json: true })
 
 console.log(payload)
