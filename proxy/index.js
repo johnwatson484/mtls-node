38b936636cfba7569fdd2f7dea2be68501aeebc1
John Watson <john.watson1@defra.gov.uk>
2025-04-04 15:18:26 +0100

    Simplify proxy

--- proxy/index.js
+++ proxy/index.js
@@ -1,89 +1,41 @@
-import tls from 'tls'
-import net from 'net'
 import fs from 'fs'
+import https from 'https'
+import net from 'net'
 
-const proxyCert = fs.readFileSync('../certificates/proxy.crt')
-const proxyKey = fs.readFileSync('../certificates/proxy-private-key.pem')
+const serverCert = fs.readFileSync('../certificates/server.crt')
+const serverKey = fs.readFileSync('../certificates/server-private-key.pem')
 const rootCA = fs.readFileSync('../certificates/rootCA.crt')
 
-const tlsOptions = {
-  key: proxyKey,
-  cert: proxyCert,
-  ca: [rootCA],
+const server = https.createServer({
+  cert: serverCert,
+  key: serverKey,
+  ca: rootCA,
   requestCert: true,
   rejectUnauthorized: true
-}
+})
 
-const server = tls.createServer(tlsOptions, (clientSocket) => {
-  console.log('Client connected to proxy')
-  let buffer = ''
+server.on('connect', (req, clientSocket, head) => {
+  console.log('Received CONNECT request for:', req.url)
+  const [targetHost, targetPort] = req.url.split(':')
 
-  clientSocket.on('data', (data) => {
-    buffer += data.toString()
-    console.log('Received data:', buffer)
+  const targetSocket = net.connect(targetPort, targetHost, () => {
+    clientSocket.write('HTTP/1.1 200 Connection Established\r\n\r\n')
+    targetSocket.write(head)
+    targetSocket.pipe(clientSocket)
+    clientSocket.pipe(targetSocket)
+  })
 
-    if (buffer.includes('\r\n\r\n')) {
-      const connectRegex = /CONNECT ([^:]+):(\d+) HTTP\/1\.1/
-      const connectMatch = connectRegex.exec(buffer)
-
-      if (connectMatch) {
-        const [, host, port] = connectMatch
-        console.log(`Valid CONNECT request for ${host}:${port}`)
-
-        const targetSocket = net.connect({
-          host,
-          port: parseInt(port, 10)
-        })
-
-        targetSocket.on('connect', () => {
-          console.log('Connected to target server')
-
-          // Send response with explicit content-length
-          const response = [
-            'HTTP/1.1 200 Connection Established',
-            'Connection: keep-alive',
-            'Content-Length: 0',
-            '',
-            ''
-          ].join('\r\n')
-
-          clientSocket.write(response, (err) => {
-            if (err) {
-              console.error('Error sending response:', err)
-              return
-            }
-            console.log('Sent response:', response)
-
-            targetSocket.pipe(clientSocket)
-            clientSocket.pipe(targetSocket)
-            console.log('Established bidirectional tunnel')
-          })
-        })
-
-        targetSocket.on('error', (err) => {
-          console.error('Target Socket Error:', err)
-          clientSocket.end()
-        })
-
-        // Remove the data handler after CONNECT
-        clientSocket.removeAllListeners('data')
-      } else {
-        console.error('Invalid CONNECT request')
-        clientSocket.end('HTTP/1.1 400 Bad Request\r\n\r\n')
-      }
-    }
+  targetSocket.on('error', (err) => {
+    console.error('Target connection error:', err)
+    clientSocket.end()
   })
 
   clientSocket.on('error', (err) => {
-    console.error('Client Socket Error:', err)
+    console.error('Client connection error:', err)
+    targetSocket.end()
   })
 })
 
-server.on('tlsClientError', (err) => {
-  console.error('TLS Client Error:', err)
-})
-
-const PORT = 3001
-server.listen(PORT, () => {
-  console.log(`Proxy server running on port ${PORT}`)
+server.listen(3001, () => {
+  console.log('Proxy server running on port 3001')
 })
