37f83485f9ea2757d031f361999d5ff519de9647
John Watson <john.watson1@defra.gov.uk>
2025-04-04 15:14:06 +0100

    Rename agent

--- proxy-client/index.js
+++ proxy-client/index.js
@@ -1,13 +1,12 @@
 import fs from 'fs'
 import tunnel from 'tunnel'
 import Wreck from '@hapi/wreck'
-import http from 'http'
 
 const clientCert = fs.readFileSync('../certificates/client.crt')
 const clientKey = fs.readFileSync('../certificates/client-private-key.pem')
 const rootCA = fs.readFileSync('../certificates/rootCA.crt')
 
-const tunnelingAgent = tunnel.httpsOverHttps({
+const agent = tunnel.httpsOverHttps({
   proxy: {
     host: 'localhost',
     port: 3001,
@@ -19,16 +18,8 @@ const tunnelingAgent = tunnel.httpsOverHttps({
   key: clientKey,
   ca: rootCA,
 })
-
-const wreck = Wreck.defaults({
-  agents: {
-    http: new http.Agent(),
-    https: tunnelingAgent,
-    httpsAllowUnauthorized: new http.Agent()
-  }
-})
-
-const { payload } = await wreck.get('https://localhost:3000', {
+const { payload } = await Wreck.get('https://localhost:3000', {
+  agent,
   json: true
 })
 
